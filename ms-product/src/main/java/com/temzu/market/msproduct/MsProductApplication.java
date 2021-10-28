5195b142908cdc1cac1e8f75511570c9dc239cd5
Temzu <40913z@mail.ru>
2021-10-28 16:41:41 +0300

    init product microservice

---
+++ ms-product/src/main/java/com/temzu/market/msproduct/MsProductApplication.java
@@ -0,0 +1,13 @@
+package com.temzu.market.msproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.temzu.market")
+public class MsProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsProductApplication.class, args);
+  }
+
+}
