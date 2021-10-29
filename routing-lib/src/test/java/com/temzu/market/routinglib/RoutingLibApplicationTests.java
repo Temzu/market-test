13e4f6b88133d84e1d1ee193d67e8cd8a49a4cfa
Temzu <40913z@mail.ru>
2021-10-29 18:39:42 +0300

    init routing library

---
+++ routing-lib/src/test/java/com/temzu/market/routinglib/RoutingLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temzu.market.routinglib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingLibApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
